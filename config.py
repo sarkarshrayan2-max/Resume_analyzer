2d48b6a1bf19fec1f728c3d882fa129e295562f0
Shrayan Sarkar <sarkarshrayan2@gmail.com>
2026-06-04 04:03:33 +0530

    Changes in Config

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 
 load_dotenv()
 
 
+def get_api_key():
+    return st.secrets.get("GROQ_API_KEY") or os.getenv("GROQ_API_KEY")
+
+
 def get_llm():
     return ChatGroq(
         model="llama-3.1-8b-instant",
         temperature=0,
-        api_key=os.getenv("GROQ_API_KEY")
+        api_key=get_api_key()
     )
